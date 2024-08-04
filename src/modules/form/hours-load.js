963d4bec4b0c1ed0e5bce9cdf015a0cacf7daaa3
Lázaro Pimentel <lazbonfim@hotmail.com>
2024-08-04 20:12:19 -0300

    Select times with submit

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -1,11 +1,49 @@
 import dayjs from "dayjs";
 import { openingHours } from "../../utils/opening-hours";
+import { hoursClick } from "./hours-click";
+
+const hours = document.getElementById("hours");
 
 export function hoursLoad({ date }) {
+  hours.innerHTML = "";
+
   const opening = openingHours.map((hour) => {
     const [scheduleHour] = hour.split(":");
 
     const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs());
-    return { scheduleHour, available: isHourPast };
+    return { hour, available: isHourPast };
   });
+
+  // Rendering timetables
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li");
+
+    li.classList.add("hour");
+    li.classList.add(available ? "hour-available" : "hour-unavailable");
+
+    li.textContent = hour;
+
+    if (hour === "9:00") {
+      hourHeaderAdd("Manhã");
+    }
+    if (hour === "13:00") {
+      hourHeaderAdd("Tarde");
+    }
+    if (hour === "18:00") {
+      hourHeaderAdd("Noite");
+    }
+
+    hours.append(li);
+  });
+
+  // Event timetables avaliables
+  hoursClick();
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li");
+  header.classList.add("hour-period");
+  header.textContent = title;
+
+  hours.append(header);
 }
--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,5 +1,6 @@
 import dayjs from "dayjs"
 const form = document.querySelector("form")
+const clientName = document.querySelector("#client")
 const selectedDate = document.querySelector("#date")
 
 const inputToday = dayjs(new Date()).format('YYYY-MM-DD')
@@ -9,4 +10,33 @@ selectedDate.min = inputToday
 
 form.onsubmit = (e) => {
     e.preventDefault()   
+
+    try {
+        const name = clientName.value.trim()
+
+        if(!name) {
+            return alert("Informe o nome do cliente!")
+        }
+
+        const hourSelected = document.querySelector(".hour-selected")
+
+        if(!hourSelected) {
+            return alert("Selecione a hora.")
+        }
+
+        const [hour] = hourSelected.innerText.split(":")
+
+        const when = dayjs(selectedDate.value).add(hour, "hour")
+
+        const id = new Date().getTime()
+
+        console.log({
+            id,
+            name,
+            when
+        })
+    } catch (error) {
+        alert("Não foi possível realizar o agendamento.")
+        console.log(error)
+    }
 }
