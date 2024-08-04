50c3d2beae216724ab1a4f426e67af55253dbdec
Lázaro Pimentel <lazbonfim@hotmail.com>
2024-08-04 18:37:14 -0300

    loading timetables

---
+++ src/modules/form/hours-load.js
@@ -0,0 +1,11 @@
+import dayjs from "dayjs";
+import { openingHours } from "../../utils/opening-hours";
+
+export function hoursLoad({ date }) {
+  const opening = openingHours.map((hour) => {
+    const [scheduleHour] = hour.split(":");
+
+    const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs());
+    return { scheduleHour, available: isHourPast };
+  });
+}
