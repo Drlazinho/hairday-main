3b1667edab996be198cea15cfce39bc725fed9d6
Lázaro Pimentel <lazbonfim@hotmail.com>
2024-08-04 21:20:50 -0300

    fetch time

--- src/modules/schedules/load.js
+++ src/modules/schedules/load.js
@@ -1,9 +1,12 @@
+import { scheduleFetchByDay } from "../../services/schedule-new.js";
 import { hoursLoad } from "../form/hours-load";
 
 const selectedDate = document.getElementById("date");
 
-export function schedulesDay() {
+export async function schedulesDay() {
   const date = selectedDate.value;
+
+  const dailySchedules = await scheduleFetchByDay({date})
 
   hoursLoad({ date });
 }
--- src/services/schedule-new.js
+++ src/services/schedule-new.js
@@ -14,3 +14,18 @@ export async function scheduleNew({ id, name, when }) {
     alert("Não foi possível agendar. Tente novamente mais tarde.");
   }
 }
+
+export async function scheduleFetchByDay({date}) {
+    try {
+        const response = await fetch(`${apiConfig.baseURL}/schedules`)
+
+        const data =  await response.json()
+
+        const dailySchedules =  data.filter((schedule) => dayjs(date).isSame(schedule.when, "day"))
+
+        return dailySchedules
+    } catch (error) {
+        console.log(error);
+        alert("Não foi possível buscar. Tente novamente mais tarde.");
+    }
+}
